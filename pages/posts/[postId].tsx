c4a87f634d8bd0283bd5e7bdc795e1decc02b335
ytakada <yuki.takada@ytakada.dev>
2022-09-12 17:50:15 +0900

    chore: 記事ページのデザイン修正など

--- pages/posts/[postId].tsx
+++ pages/posts/[postId].tsx
@@ -1,28 +1,41 @@
 import { NextPage } from "next";
-import styles from "../../styles/pages/Post.module.scss";
+import styles from "../../styles/pages/posts/Post.module.scss";
 import Head from "next/head";
+import Image from "next/image";
 
 const PostPage: NextPage<any> = ({ post }: { post: any }) => {
   return (
     <>
       <Head>
         <title>{post.title}</title>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/themes/prism-tomorrow.min.css" rel="stylesheet" />
       </Head>
 
-      <article className={styles.post}>
-        <h1 className={styles.title}>{post.title}</h1>
-        <div className={styles.dates}>
-          <div className={styles.posted_at}>
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none" /><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" /></svg>
-            <p>{post.posted_at}</p>
+      <main className={styles.main}>
+        <article className={styles.post}>
+          <h1 className={styles.title}>{post.title}</h1>
+
+          <div className={styles.meta}>
+            <div className={styles.post_date}>
+              <div className={styles.icon_post}>
+                <Image src="/edit_FILL0_wght400_GRAD0_opsz48.svg" layout="fill" alt="icon_post" />
+              </div>
+              <p className={styles.label}>{post.posted_at}</p>
+            </div>
+            <div className={styles.update_date}>
+              <div className={styles.icon_update}>
+                <Image src="/update_FILL0_wght400_GRAD0_opsz48.svg" layout="fill" alt="icon_update" />
+              </div>
+              <p className={styles.label}>{post.updated_at}</p>
+            </div>
           </div>
-          <div className={styles.updated_at}>
-            <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><g><rect fill="none" height="24" width="24" /></g><g><g><path d="M11,8v5l4.25,2.52l0.77-1.28l-3.52-2.09V8H11z M21,10V3l-2.64,2.64C16.74,4.01,14.49,3,12,3c-4.97,0-9,4.03-9,9 s4.03,9,9,9s9-4.03,9-9h-2c0,3.86-3.14,7-7,7s-7-3.14-7-7s3.14-7,7-7c1.93,0,3.68,0.79,4.95,2.05L14,10H21z" /></g></g></svg>
-            <p>{post.updated_at}</p>
-          </div>
-        </div>
-        <div className={styles.content} dangerouslySetInnerHTML={{ __html: post.content }} />
-      </article>
+
+          <div className={styles.content} dangerouslySetInnerHTML={{ __html: post.content }} />
+        </article>
+      </main>
+
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/prism.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/plugins/autoloader/prism-autoloader.min.js"></script>
     </>
   );
 }
