039f5bc9821dfeef238eae7f9c07ad93c84b4244
ytakada <yuki.takada@ytakada.dev>
2022-09-13 12:30:19 +0900

    chore: スマホ表示崩れの修正

--- components/header.tsx
+++ components/header.tsx
@@ -5,15 +5,17 @@ import styles from "../styles/components/Header.module.scss";
 export default function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.icon}>
-        <Link href="/">
-          <a>
-            <Image src="/logo.svg" alt="logo" width="100%" height="100%" />
-          </a>
-        </Link>
-      </div>
-      <div className={styles.breadcrumbs}>
-        <p>ytakada.dev</p>
+      <div className={styles.container}>
+        <div className={styles.icon}>
+          <Link href="/">
+            <a>
+              <Image src="/logo.svg" alt="logo" width="100%" height="100%" />
+            </a>
+          </Link>
+        </div>
+        <div className={styles.breadcrumbs}>
+          <p>ytakada.dev</p>
+        </div>
       </div>
     </header>
   );
